9c19f0a9da3f1b2f17a79753c8f9b4d9cbfe8d6a
Rayon Hunte <rayon.hunte@gmail.com>
2024-09-22 21:33:57 -0400

    export component test

---
+++ apps/web/src/app/table/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Table } from '@repo/ui/table';
+
+
+const Page: React.FC = () => {
+    return (
+        <div>
+            <h1>Table Page</h1>
+            <p>This is the table page component.</p>
+            <Table />
+        </div>
+    );
+};
+
+export default Page;
